39215da3de5d8e32c269f4094e5c0e7ef3693676
WangJunnan <991851610@qq.com>
2019-09-27 09:37:02 +0800

    feat: update json util

--- src/main/java/com/walm/common/util/JsonUtils.java
+++ src/main/java/com/walm/common/util/JsonUtils.java
@@ -1,11 +1,11 @@
 package com.walm.common.util;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializeFilter;
-import com.alibaba.fastjson.serializer.SerializerFeature;
-
-import java.util.List;
-
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lombok.Data;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 /**
  * JsonUtils
  *
@@ -14,6 +14,16 @@ import java.util.List;
  */
 public class JsonUtils {
 
+    private static final Gson gson = new GsonBuilder()
+            // 不过滤空值
+            .serializeNulls()
+            // 设置字段命名转换规则 --- 驼峰 eg: user_name -> userName userName -> user_name
+            //.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            // 设置字段序列化/反序列化过滤规则
+            .excludeFieldsWithModifiers(Modifier.TRANSIENT, Modifier.STATIC)
+            .create();
+
+
     /**
      * 对象序列化Json字符串
      *
@@ -21,18 +31,7 @@ public class JsonUtils {
      * @return
      */
     public static String toJson(Object obj) {
-        return JSON.toJSONString(obj);
-    }
-
-    /**
-     * 对象序列化Json字符串
-     *
-     * @param obj
-     * @param features
-     * @return
-     */
-    public static String toJson(Object obj, SerializeFilter serializeFilter, SerializerFeature... features) {
-        return JSON.toJSONString(obj,serializeFilter, features);
+        return gson.toJson(obj);
     }
 
     /**
@@ -43,16 +42,33 @@ public class JsonUtils {
      * @return
      */
     public static <T> T fromJson(String json, Class<T> clazz) {
-        return JSON.parseObject(json, clazz);
+        return gson.fromJson(json, clazz);
     }
 
     /**
-     * Json字符串反序列化成List
+     * Json字符串反序列化成泛型对象
+     *
+     * eg: List<String> ->  List<String> ss = </>fromJson(json, new TypeToken<List<String>>() {}.getType())
+     *     Demo<A> ->  Demo<A> de = fromJson(json, new TypeToken<Demo<A>>() {}.getType())
+     *
      * @param json
-     * @param clazz
+     * @param type
      * @return
      */
-    public static <T> List<T> fromJsonArray(String json, Class<T> clazz){
-        return JSON.parseArray(json, clazz);
+    public static <T> T fromJson(String json, Type type) {
+        return gson.fromJson(json, type);
+    }
+
+    public static void main(String[] args) {
+        String s = "{\"user_name\":\"wangjunnan\",\"age_ad\":\"ds\"}";
+        A a = fromJson(s, A.class);
+        System.out.println(a);
+        System.out.println(toJson(a));
+    }
+
+    @Data
+    public static class A {
+        private String userName;
+        private String ageAd;
     }
 }
